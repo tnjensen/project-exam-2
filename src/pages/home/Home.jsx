333b0ae3e946b1735f856da1701fc4d5f494d67b
Thomas N. Jensen <thomasnorupjensen@gmail.com>
2024-04-20 12:47:49 +0200

    Added user info to posts

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,11 +1,12 @@
 import Posts from '../../components/posts/Posts';
+import MyShare from '../../components/share/MyShare';
 import Share from '../../components/share/Share';
 import './home.scss';
 
 function Home(){
     return(
         <div className='home'>
-            <Share />
+            <MyShare />
             <Posts />
         </div>
         
