5b4ef139574e4efbdb4c0da4cb50548dfb0e63a2
Thomas N. Jensen <thomasnorupjensen@gmail.com>
2024-06-19 08:52:25 +0200

    Added netlify redirects

--- src/components/posts/Posts.jsx
+++ src/components/posts/Posts.jsx
@@ -6,10 +6,13 @@ import { useToken } from "../../stores/useUserStore.jsx";
 import Share from "../share/Share";
 import AddOutlinedIcon from "@mui/icons-material/AddOutlined";
 import RemoveOutlinedIcon from "@mui/icons-material/RemoveOutlined";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import { SearchOutlined } from "@mui/icons-material";
 
 function Posts() {
   const [shareOpen, setShareOpen] = useState(false);
+  const [searchInput, setSearchInput] = useState("");
+  const [filteredResults, setFilteredResults] = useState([]);
   const token = useToken();
   const {
     data: posts,
@@ -18,11 +21,30 @@ function Posts() {
   } = useApi(apiUrl + `?_author=true&_comments=true&_reactions=true`, token);
   console.log(posts);
 
+  useEffect(() => {
+    setFilteredResults(filteredResults);
+  }, [filteredResults])
+
   if (isLoading) {
     return <div></div>;
   }
   if (isError) {
     return <div>Error loading posts.</div>;
+  }
+
+  
+  const searchItems = (searchValue) => {
+    setSearchInput(searchValue);
+    if (searchInput !== '') {
+        const filteredData = posts.filter((item) => {
+            return Object.values(item).join('').toLowerCase().includes(searchInput.toLowerCase())
+        })
+        setFilteredResults(filteredData)
+        console.log(filteredData)
+    }
+    else{
+        setFilteredResults(posts)
+    }
   }
   return (
     <div className="posts">
@@ -40,10 +62,23 @@ function Posts() {
           onClick={() => setShareOpen(!shareOpen)}
         />
       )} */}
+      <div className="search">
+              <SearchOutlined />
+              <input
+                type="text"
+                placeholder="Search.."
+                onChange={(e) => searchItems(e.target.value)}
+              />
+            </div>
       <Share />
-      {posts.map((post) => (
+      {searchInput.length > 1 ? (
+                filteredResults.map((post) => (
+                  post.media && <Post key={post.id} post={post} />
+                ))
+            ) : (
+        posts.map((post) => (
         post.media && <Post key={post.id} post={post} />
-      ))}
+      )))}
     </div>
   );
 }
