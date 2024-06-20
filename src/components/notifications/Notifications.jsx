46769bfd697e95500bbf65a5f2cc9d7d3ed74d6f
Thomas N. Jensen <thomasnorupjensen@gmail.com>
2024-06-20 11:51:23 +0200

    Edited layout

--- src/components/notifications/Notifications.jsx
+++ src/components/notifications/Notifications.jsx
@@ -4,11 +4,12 @@ function Notifications(){
     return(
         <div className="notifications-content">
             <h3>Your latest messages</h3>
-            <div className='messages'>
-                <ul className='message-list'>
+            {/* <div className='messages'> */}
+                <ul className='messages'>
+                    <li>Friend message</li>
                     <li>Friend message</li>
                 </ul>
-           </div>
+           {/* </div> */}
         </div>
     )
 }
