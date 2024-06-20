cb261eebd777e6f9d0cb27e807e62efbb5caf1da
Thomas N. Jensen <thomasnorupjensen@gmail.com>
2024-06-20 20:59:29 +0200

    Refactored edit profile details

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -4,15 +4,12 @@ import LeftBar from "./leftbar/LeftBar";
 import RightBar from "./rightbar/Rightbar";
 
 const Layout = () => {
-  const partUrl = window.location.href.split("/").pop();
   
   return (
     <div className="wrapper">
       <Header />
       <div style={{ display: "flex" }}>
-          {/* {partUrl !== 'navigation' &&  */}
             <LeftBar />
-         {/*  } */}
         <div style={{ flex: 6 }}>
           <Outlet />
         </div>
